4f5d0e8febc410803092aaf3876d193bb14fcab9
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-05-02 14:43:50 +0000

    [Finish]応用課題5

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,29 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: {  maximum: 50 }
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: {  maximum: 50 }
+
   def is_followed_by?(user)
     reverse_of_relationships.find_by(following_id: user.id).present?
+  end
+
+#検索方法の分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE ?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE ?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE ?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE ?", "%#{word}%")
+    else
+      @user = User.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,7 @@ Rails.application.routes.draw do
      get :followers, on: :member
   end
 
+  get "search" => "searches#search",as: "search"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
