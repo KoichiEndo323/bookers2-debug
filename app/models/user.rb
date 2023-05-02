6246110098f5b3de11c6ff509a758cfe1f3fb373
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-05-02 07:29:40 +0000

    [Start]応用課題5

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   has_many :book_comments,dependent: :destroy
 
   #フォローする側
-  has_many :relationships,foreign_key: :following_id
+  has_many :relationships, class_name:'Relationship',foreign_key: :following_id, dependent: :destroy
   #フォローしている全員のデータをとってくる。中間テーブルを介して、followerテーブルから
   has_many :followings,through: :relationships,source: :follower
 
   #フォローされる側
-  has_many :reverse_of_relationships,class_name:'Relationship',foreign_key: :follower_id
+  has_many :reverse_of_relationships,class_name:'Relationship',foreign_key: :follower_id, dependent: :destroy
   #フォローされている全員のデータをとってくる。中間テーブルを介して、follwingから
   has_many :followers,through: :reverse_of_relationships,source: :following
 
