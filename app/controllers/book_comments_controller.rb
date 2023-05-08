75a8e6f2d0dba2f97afb5e9829a4f96d8b31d6eb
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-05-08 16:27:32 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-   @book = Book.find(params[:book_id])
-   @comment = current_user.book_comments.new(book_comment_params)
-   @comment.book_id = @book.id
-   @comment.save
-   @book_comment = BookComment.new(book_id: @book.id)
-   render 'book_comments/_replace'
+    book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
+    render 'book_comments/_comments'
   end
 
   def destroy
-   @book = Book.find(params[:book_id])
-   BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-   render 'book_comments/_replace'
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    render 'book_comments/_comments'
   end
 
   private
-
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-
-
 end
