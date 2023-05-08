b0cdf5ae53299d6a81cc3b90e8b4b219929264d5
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-05-08 05:08:03 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,10 +4,14 @@ class BookCommentsController < ApplicationController
    @comment = current_user.book_comments.new(book_comment_params)
    @comment.book_id = @book.id
    @comment.save
+   @book_comment = BookComment.new(book_id: @book.id)
+   render 'book_comments/_replace'
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+   @book = Book.find(params[:book_id])
+   BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+   render 'book_comments/_replace'
   end
 
   private
