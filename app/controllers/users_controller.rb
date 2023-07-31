370ee9ed0fd84f849dfb9929ab2622b83f192683
KoichiEndo323 <waddledee323@gmail.com>
2023-07-31 13:33:51 +0000

    [add]lesson8_b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,15 @@ class UsersController < ApplicationController
         @entry = Entry.new
       end
     end
+
+    #投稿数の表示
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
+    @the_day_before =   @today_book.count / @yesterday_book.count.to_f
   end
+
 
   def index
     @users = User.all
@@ -43,12 +51,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user =User.find(params[:id])
-
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
--- app/models/book.rb
+++ app/models/book.rb
@@ -16,7 +16,17 @@ class Book < ApplicationRecord
     favorites.where(created_at: 1.week.ago..Time.zone.now).count
   end
 
+#投稿数の集計日時
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
+#日数を指定できる定義
+  (0..6).each do |days|
+  scope "created_#{days}day_ago".to_sym, -> { where(created_at: days.days.ago.all_day) }
+end
 
+  
 #検索方法の分岐
     def self.looks(search, word)
       if search == "perfect"
